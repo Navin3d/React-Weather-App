51149544ab8f1df840db0e0ea2e1b4899de0b54e
Navin3d <smnavin65@gmail.com>
2022-01-21 22:45:47 +0530

    Changed Contact Url

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,8 +5,8 @@ const ContactPage = () => {
         <Fragment>
             <h1 className="page-title">This is Contact Page...</h1>
             <ol>
-                <li>Link to <a href="">Github</a></li>
-                <li>Link to <a href="">Vs-Code</a></li>
+                <li>Link to <a href="https://github.com/Navin3d/React-Weather-App">Github</a></li>
+                <li>Link to <a href="https://vscode.dev/github.com/Navin3d/React-Weather-App">Vs-Code</a></li>
             </ol>
         </Fragment>
     )
